5654031e30131184ae1e191edc1fc6ac621c0f9c
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-12-11 09:05:55 -0800

    tasks new1

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,8 +12,6 @@ class FileStorage:
 
     def all(self):
         """returns the __objects dictionary"""
-        print(type(FileStorage.__objects))
-        print(FileStorage.__objects)
         return FileStorage.__objects
 
     def new(self, obj):
@@ -23,10 +21,6 @@ class FileStorage:
 
     def save(self):
         """serializes __objects into json file with path __file_path"""
-        """dict1 = {}
-        for key in FileStorage.__objects.keys():
-            dict1[key] = (FileStorage.__objects[key].to_dict())
-        str1 = json.dumps(dict1)"""
         with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
             json.dump(FileStorage.__objects, f)
 
@@ -35,11 +29,6 @@ class FileStorage:
         try:
             with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
                 FileStorage.__objects = json.load(f)
-                print(type(FileStorage.__objects))
-                print(FileStorage.__objects)
         except FileNotFoundError:
             return
-        """for o in dict1.values():
-            cls_name = o["__class__"]
-            del o["__class__"]
-            self.new(eval(cls_name)(**o))"""
+
