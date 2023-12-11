34772f6b1146d6ae1c282b90d5556305f572105b
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-12-11 09:03:14 -0800

    tasks new

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,30 +12,34 @@ class FileStorage:
 
     def all(self):
         """returns the __objects dictionary"""
+        print(type(FileStorage.__objects))
+        print(FileStorage.__objects)
         return FileStorage.__objects
 
     def new(self, obj):
         """sets a new obj in __objects dictionary"""
         FileStorage.__objects["{}.{}".format(obj.__class__.__name__,
-                                             obj.id)] = obj
+                                             obj.id)] = obj.to_dict()
 
     def save(self):
         """serializes __objects into json file with path __file_path"""
-        dict1 = {}
+        """dict1 = {}
         for key in FileStorage.__objects.keys():
             dict1[key] = (FileStorage.__objects[key].to_dict())
-        str1 = json.dumps(dict1)
+        str1 = json.dumps(dict1)"""
         with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
-            f.write(str1)
+            json.dump(FileStorage.__objects, f)
 
     def reload(self):
         """deserializes file to __objects"""
         try:
             with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
-                dict1 = json.load(f)
+                FileStorage.__objects = json.load(f)
+                print(type(FileStorage.__objects))
+                print(FileStorage.__objects)
         except FileNotFoundError:
             return
-        for o in dict1.values():
+        """for o in dict1.values():
             cls_name = o["__class__"]
             del o["__class__"]
-            self.new(eval(cls_name)(**o))
+            self.new(eval(cls_name)(**o))"""
--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -3,6 +3,9 @@ from models import storage
 from models.base_model import BaseModel
 
 all_objs = storage.all()
+print("Hello!!!")
+print(all_objs)
+print(type(all_objs))
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
     obj = all_objs[obj_id]
@@ -12,5 +15,9 @@ print("-- Create a new object --")
 my_model = BaseModel()
 my_model.name = "My_First_Model"
 my_model.my_number = 89
+print(my_model.__dict__)
+print(type(my_model.__dict__))
+print(my_model.__dict__.copy())
+print(type(my_model.__dict__.copy()))
 my_model.save()
 print(my_model)
